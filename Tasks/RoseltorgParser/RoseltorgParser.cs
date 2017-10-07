54af53664298b10f5d9406be1a1ff6c2d8210920
zv <zv@itworks24.ru>
2017-10-07 21:24:35 +0300

    Fixed some bugs

---
+++ Tasks/RoseltorgParser/RoseltorgParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using HtmlAgilityPack;
+using Sisyphus.Tasks;
+
+namespace Sysiphus.Tasks
+{
+    public partial class RoseltorgParser
+    {
+        public bool ExecuteProcess()
+        {
+            var settings = GetSettings(typeof(Settings.RoseltorgSettings)) as Settings.RoseltorgSettings;
+
+            var url = "https://www.roseltorg.ru/procedures/search/";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            var nodes = doc.DocumentNode.SelectNodes("//a").Where(x => x.Attributes["class"].Value == "g-link");
+            foreach (var node in nodes)
+            {
+                CreateLogRecord(node.Attributes["href"].Value); 
+            }
+            return true;
+
+        }
+    }
+
+    public partial class RoseltorgParser : TaskBase
+    {
+        public override ExecutionDelegate ExecutuionMethod => ExecuteProcess;
+        public override IEnumerable<Type> TaskSettings => new[] { typeof(Settings.RoseltorgSettings) };
+    }
+}
---
+++ Tasks/RoseltorgParser/RoseltorgParserSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using Sisyphus.Settings;
+
+namespace Sysiphus.Tasks.Settings
+{
+    class RoseltorgSettings : SettingsRepresent
+    {
+
+        [Category("Target")]
+        [DisplayName("Directory")]
+        public string SampleProperty { get; set; } = @"\roseltorg";
+
+        public RoseltorgSettings(string groupName) : base(groupName)
+        {
+        }
+    }
+}
